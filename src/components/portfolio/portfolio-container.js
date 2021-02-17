3b6d97b6606c07426bf5ac6386788f1533678e08
Tyler <Gosselin>
2021-02-17 11:19:17 -0700

    How to Build a Data Filter in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 
-import PortfolioItem from "./portfolio-item"
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
   constructor() {
-    super ();
+    super();
 
     this.state = {
       pageTitle: "Welcome to my portfolio",
       data: [
-        {title: "Vestas"},
-        {title: "Cargill"},
-        {title: "Army"},
-        {title:  "Handy Vets"}
-      ]
-    }
+        { title: "Quip", category: "eCommerce" },
+        { title: "Eventbrite", category: "Scheduling" },
+        { title: "Ministry Safe", category: "Enterprise" },
+        { title: "SwingAway", category: "eCommerce" },
+      ],
+    };
 
-    this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
-  }
-  
-  portfolioItems() {
-    return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={"google.com"}/>;
-    })
+    this.handleFilter = this.handleFilter.bind(this);
   }
 
-  handlePageTitleUpdate() {
+  handleFilter(filter) {
     this.setState({
-      pageTitle: "Something Else"
-    })
+      data: this.state.data.filter((item) => {
+        return item.category === filter;
+      }),
+    });
+  }
+
+  portfolioItems() {
+    return this.state.data.map((item) => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
   }
 
   render() {
@@ -36,12 +38,18 @@ export default class PortfolioContainer extends Component {
       <div>
         <h2>{this.state.pageTitle}</h2>
 
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+
         {this.portfolioItems()}
-
-        <hr />
-
-        <button onClick={this.handlePageTitleUpdate}>Change Title</button>
       </div>
-    )
+    );
   }
 }
