60bf1b4651dda65391f93a6942f34647d1fa8e12
Tyler <Gosselin>
2021-02-17 13:46:39 -0700

    Guide to Working with NavLink Active Styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -12,11 +12,16 @@ export default class NavigationComponent extends Component {
         <NavLink exact to="/">
           Home
         </NavLink>
+        <NavLink to="/about-me" activeClassName="nav-link-active">
+          About
+        </NavLink>
+        <NavLink to="/contact" activeClassName="nav-link-active">
+          Contact
+        </NavLink>
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
 
-        <NavLink to="/about-me">About</NavLink>
-
-        <button>Contact</button>
-        <button>Blog</button>
         {true ? <button>Add Blog</button> : null}
       </div>
     );
