7cae9ddd1c947b44889ad9ae6027757cd119cdef
Tyler <Gosselin>
2021-02-17 16:54:22 -0700

    Making the Initial API GET Request in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import moment from "moment";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -10,9 +11,27 @@ import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match";
 
-
 export default class App extends Component {
+  constructor(){
+    super();
+
+    this.getPortfolioItems=this.getPortfolioItems.bind(this)
+  }
+  getPortfolioItems() {
+    axios
+      .get("https://tylergosselin.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        // handle success
+        console.log("response data", response);
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      });
+  }
+
   render() {
+    this.getPortfolioItems();
     return (
       <div className="app">
         <Router>
@@ -26,7 +45,11 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/Blog" component={Blog} />
-              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
               <Route component={NoMatch} />
             </Switch>
           </div>
