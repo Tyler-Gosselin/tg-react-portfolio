b2ae26346c328b146be64f8b100355f3284c2e08
Tyler <gosselin.code@gmail.com>
2021-02-25 14:50:26 -0700

    Building an Edit Click Handler in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,8 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const PortfolioSidebarList = (props) => {
-  const portfolioList = props.data.map((portfolioItem) => {
+const PortfolioSidebarList = props => {
+  const portfolioList = props.data.map(portfolioItem => {
     return (
       <div key={portfolioItem.id} className="portfolio-item-thumb">
         <div className="portfolio-thumb-img">
@@ -11,12 +11,22 @@ const PortfolioSidebarList = (props) => {
 
         <div className="text-content">
           <div className="title">{portfolioItem.name}</div>
-          <a
-            className="delete-icon"
-            onClick={() => props.handleDeleteClick(portfolioItem)}
-          >
-            <FontAwesomeIcon icon="trash" />
-          </a>
+
+          <div className="actions">
+            <a
+              className="action-icon"
+              onClick={() => props.handleEditClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="edit" />
+            </a>
+
+            <a
+              className="action-icon"
+              onClick={() => props.handleDeleteClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
         </div>
       </div>
     );
