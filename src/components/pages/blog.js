0795db528d89e33180e23cd80090ea24cff67cba
Tyler <gosselin.code@gmail.com>
2021-03-01 15:12:32 -0700

    Rendering Blog Records to the Screen

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function () {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://tylergosselin.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <h1>{blogItem.title}</h1>
+    })
+
+    return (
       <div>
-        <Link to="/about-me">Read more about me</Link>
+        {blogRecords}
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
