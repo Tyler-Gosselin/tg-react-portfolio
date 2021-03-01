897fc043634b0ce61a94a8f5b7c65afef5cd2b5c
Tyler <gosselin.code@gmail.com>
2021-03-01 16:45:02 -0700

    Getting the Count of Total Records on the Server and Updating the Component State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -9,10 +9,12 @@ class Blog extends Component {
 
     this.state = {
       blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll()
+    this.activateInfiniteScroll();
   }
 
   activateInfiniteScroll() {
@@ -27,6 +29,10 @@ class Blog extends Component {
   }
 
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+    });
+
     axios
       .get("https://tylergosselin.devcamp.space/portfolio/portfolio_blogs", {
         withCredentials: true,
@@ -34,6 +40,7 @@ class Blog extends Component {
       .then((response) => {
         this.setState({
           blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records,
         });
       })
       .catch((error) => {
