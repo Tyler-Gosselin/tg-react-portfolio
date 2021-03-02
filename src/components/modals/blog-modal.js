7cf43290d5881fb4ea74cf3a67a9538d193df834
Tyler <gosselin.code@gmail.com>
2021-03-02 13:56:22 -0700

    Triggering the React Modal to Open with a Link Click

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -8,7 +8,7 @@ class BlogModal extends Component {
   }
   render() {
     return (
-      <ReactModal isOpen={true}>
+      <ReactModal isOpen={this.props.modalIsOpen}>
         <h1>Im in a modal!</h1>
       </ReactModal>
     );
